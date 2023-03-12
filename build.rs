217547a4fe07bd4348a0d362b77169a9557821fa
Rob Pilling <robpilling@gmail.com>
2023-03-12 20:29:27 +0000

    Update offline builds

--- build.rs
+++ build.rs
@@ -1,4 +1,20 @@
+use std::env::{self, VarError};
+
 fn main() {
-    println!("cargo:rustc-env=DATABASE_URL=sqlite://pod.sql");
-    println!("cargo:rerun-if-changed=migrations");
+    let use_db = |_url| {
+        // println!("cargo:rustc-env=DATABASE_URL={}", url);
+        println!("cargo:rerun-if-changed=migrations");
+    };
+
+    let use_json = || println!("cargo:rustc-env=SQLX_OFFLINE=true");
+
+    println!("cargo:rerun-if-env-changed=DATABASE_URL");
+    match env::var("DATABASE_URL") {
+        Ok(url) => use_db(url),
+        Err(VarError::NotPresent) => {
+            println!("cargo:warning={}", "using `sqlx-data.json` for schema");
+            use_json()
+        },
+        Err(e) => panic!("$DATABASE_URL: {e:?}"),
+    }
 }
