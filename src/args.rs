5d1ed19575deed6b5d4447d8ad40fc0b0dd2f071
Rob Pilling <robpilling@gmail.com>
2023-03-12 21:43:11 +0000

    Document arguments and s/local/any-address/

--- src/args.rs
+++ src/args.rs
@@ -2,22 +2,27 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Args {
+    /// Whether podsync's clients connect to it over https.
+    /// If so, the sessionid cookie is sent as a secure cookie.
     #[arg(short, long)]
     secure: bool,
 
+    /// Whether podsync listens on the wildcard address. By default
+    /// podsync will listen just on the loopback.
     #[arg(short, long)]
-    local: bool,
+    any_address: bool,
 
+    /// The port podsync listens on.
     #[arg(short, long, default_value_t = 80)]
     port: u16,
 }
 
 impl Args {
     pub fn addr(&self) -> impl Into<std::net::SocketAddr> {
-        if self.local {
-            ([127, 0, 0, 1], self.port)
-        } else {
+        if self.any_address {
             ([0, 0, 0, 0], self.port)
+        } else {
+            ([127, 0, 0, 1], self.port)
         }
     }
 
