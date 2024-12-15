5ba83d3631359fd043073042fb5b90e97284126e
Rob Pilling <robpilling@gmail.com>
2024-12-15 16:50:42 +0000

    auth.rs: return `AuthError`, emit error on BasicAuth parsing

--- src/auth.rs
+++ src/auth.rs
@@ -1,14 +1,20 @@
-use std::str::FromStr;
+use std::{
+    fmt::{self, Display, Formatter},
+    str::FromStr,
+};
 
 use base64_light::base64_decode;
 use log::error;
 use uuid::Uuid;
 
-use crate::podsync;
-
 pub struct BasicAuth {
     user: String,
     pass: String,
+}
+
+pub struct AuthError<'u> {
+    path_user: &'u str,
+    auth_user: String,
 }
 
 pub struct AuthAttempt {
@@ -23,10 +29,25 @@ pub fn pwhash(s: &str) -> String {
 }
 
 impl BasicAuth {
-    pub fn with_path_username(self, username: &str) -> podsync::Result<AuthAttempt> {
-        (self.user == username)
-            .then(|| AuthAttempt { auth: self })
-            .ok_or(podsync::Error::Unauthorized)
+    pub fn with_path_username(self, username: &str) -> Result<AuthAttempt, AuthError<'_>> {
+        if self.user == username {
+            Ok(AuthAttempt { auth: self })
+        } else {
+            Err(AuthError {
+                path_user: username,
+                auth_user: self.user,
+            })
+        }
+    }
+}
+
+impl Display for AuthError<'_> {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(
+            fmt,
+            "path-username ({:?}) <-> auth-username ({:?}) mismatch",
+            self.path_user, self.auth_user
+        )
     }
 }
 
@@ -34,24 +55,31 @@ impl FromStr for BasicAuth {
     type Err = &'static str;
 
     fn from_str(header: &str) -> Result<Self, Self::Err> {
-        let (basic, auth_b64) = header.split_once(' ').ok_or("no space in auth header")?;
+        let inner = || {
+            let (basic, auth_b64) = header.split_once(' ').ok_or("no space in auth header")?;
 
-        if basic != "Basic" {
-            return Err("only basic auth supported");
-        }
+            if basic != "Basic" {
+                return Err("only basic auth supported");
+            }
 
-        let auth_bytes = base64_decode(auth_b64);
-        let auth = std::str::from_utf8(&auth_bytes).map_err(|e| {
-            error!("invalid utf-8 for password: {e:?}");
-            "none-utf8 in auth header"
-        })?;
+            let auth_bytes = base64_decode(auth_b64);
+            let auth = std::str::from_utf8(&auth_bytes).map_err(|e| {
+                error!("invalid utf-8 for password: {e:?}");
+                "none-utf8 in auth header"
+            })?;
 
-        let (user, pass) = auth.split_once(':').ok_or("no colon in auth value")?;
+            let (user, pass) = auth.split_once(':').ok_or("no colon in auth value")?;
 
-        let user = user.into();
-        let pass = pass.into();
+            let user = user.into();
+            let pass = pass.into();
 
-        Ok(Self { user, pass })
+            Ok(Self { user, pass })
+        };
+
+        inner().map_err(|e| {
+            error!("{}", e);
+            e
+        })
     }
 }
 
